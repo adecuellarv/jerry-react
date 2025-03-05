35e3951ee431bfe391a3205c8822205fb8af444d
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2025-03-04 20:59:49 -0600

    hover modal cafetera

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -1,7 +1,11 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import "./styles.css";
+import "./button.css";
 import cafeteria from './img/CAFETERA.jpg'
+import { useEffect } from "react";
 
 const getImage = (text) => {
   switch (text) {
@@ -22,19 +26,37 @@ const getTitle = (text) => {
 }
 
 export default function Modal({ setShowModal, typeModal = 'cafeteria', }) {
+  const [open, setOpen] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+    setTimeout(() => {
+      setShowModal(false)
+    }, 1200);
+
+  }
+
+  useEffect(() => {
+    if (open) {
+      setTimeout(() => {
+        setShowButton(true);
+      }, 800);
+    } else setShowButton(false);
+  }, [open]);
 
   return (
     <div className="overlay">
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
+        initial={{ opacity: open ? 0 : 1 }}
+        animate={{ opacity: open ? 1 : 0 }}
+        exit={{ opacity: open ? 0 : 1 }}
         className="modal-content"
       >
         <motion.div
-          initial={{ y: -100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.9 }}
+          initial={{ y: open ? -300 : 0, opacity: open ? 0 : 1 }}
+          animate={{ y: open ? 0 : -300, opacity: open ? 1 : 0 }}
+          transition={{ duration: .9 }}
           style={{
             backgroundImage: `url(${getImage(typeModal)})`,
             backgroundRepeat: "no-repeat",
@@ -49,19 +71,23 @@ export default function Modal({ setShowModal, typeModal = 'cafeteria', }) {
         </motion.div>
 
         <motion.div
-          initial={{ y: 100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.9 }}
+          initial={{ y: open ? 300 : 0, opacity: open ? 0 : 1 }}
+          animate={{ y: open ? 0 : 300, opacity: open ? 1 : 0 }}
+          transition={{ duration: .9 }}
           className="right-panel"
         >
           <h2>Descripción</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </motion.div>
       </motion.div>
-
-      <button onClick={() => setShowModal(false)} className="close-button">
-        Cerrar
-      </button>
+      {showButton &&
+        <div className="div-close-btn">
+          <button className="close-button" onClick={handleClose}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+          <span>Cerrar</span>
+        </div>
+      }
     </div>
   );
 }
